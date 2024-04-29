207c94059dd0a63a06188ad1065f330dcaef018f
JericoL <Jericolabajo11@gmail.com>
2024-04-29 22:29:47 +0800

    Api For maps installed

--- Location_App_Labajo/MainActivity.cs
+++ Location_App_Labajo/MainActivity.cs
@@ -1,25 +1,58 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
+using Android.Gms.Maps;
 using Android.OS;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
 
+
 namespace Location_App_Labajo
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
     {
+        //permission
+        readonly string[] permissionGroup =
+        {
+          Manifest.Permission.AccessCoarseLocation,
+          Manifest.Permission.AccessFineLocation
+        };
+
+     
+        //object for googlemaps
+        GoogleMap map;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            RequestPermissions(permissionGroup, 0);
+
+            //referencing the map  fragment
+            SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
         }
+
+   
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            map = googleMap;
+
+            map.UiSettings.ZoomControlsEnabled = true;
+        }
+
+
+   
     }
 }
